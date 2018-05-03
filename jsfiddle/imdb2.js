37d172e7b408cef752112e4e4b10ccdb23a67063
sunny chouhan <38137692+snychn01@users.noreply.github.com>
2018-05-03 13:52:16 +0530

    updated
    updated

--- jsfiddle/imdb2.js
+++ jsfiddle/imdb2.js
@@ -1,1 +1,49 @@
 
+var rows = [
+  {
+    "reviews_count": 4446,
+    "runtime": 142,
+    "country": [
+      "USA"
+    ],
+    "tags": [
+      "prison",
+      "wrongful imprisonment",
+      "escape from prison",
+      "prison cell search",
+      "first person narration"
+    ],
+    "genres": [
+      "Crime",
+      "Drama"
+    ],
+    "actors": [
+      "Tim Robbins",
+      "Morgan Freeman",
+      "Bob Gunton",
+      "William Sadler",
+      "Clancy Brown",
+      "Gil Bellows",
+      "Mark Rolston",
+      "James Whitmore",
+      "Jeffrey DeMunn",
+      "Larry Brandenburg",
+      "Neil Giuntoli",
+      "Brian Libby",
+      "David Proval",
+      "Joseph Ragno",
+      "Jude Ciccolella"
+    ],
+    "name": "The Shawshank Redemption",
+    "year": 1994,
+    "rating": 9.3,
+    "votes": 1790841,
+    "image": "https://s3-eu-west-1.amazonaws.com/example-items/movies/5940bf19-4874-41a0-b458-30b5de9ca39b-orig.jpg",
+    "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "storyline": "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red. Written by J-S-Golden",
+    "director": [
+      "Frank Darabont"
+    ]
+ 
+  }
+]
