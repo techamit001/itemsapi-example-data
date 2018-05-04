2b58f6577b0da550d55002b63141858032f28c8a
sunny chouhan <38137692+snychn01@users.noreply.github.com>
2018-05-04 09:18:49 +0530

    Update imdb2.js

--- jsfiddle/imdb2.js
+++ jsfiddle/imdb2.js
@@ -4,46 +4,29 @@ var rows = [
     "reviews_count": 4446,
     "runtime": 142,
     "country": [
-      "USA"
+      "India"
     ],
     "tags": [
-      "prison",
-      "wrongful imprisonment",
-      "escape from prison",
-      "prison cell search",
-      "first person narration"
+      "men",
+      "shirt",
+      "denim",
+      "white"
     ],
-    "genres": [
-      "Crime",
-      "Drama"
+    
+    "type": [
+      "formal",
+      "casual"
     ],
-    "actors": [
-      "Tim Robbins",
-      "Morgan Freeman",
-      "Bob Gunton",
-      "William Sadler",
-      "Clancy Brown",
-      "Gil Bellows",
-      "Mark Rolston",
-      "James Whitmore",
-      "Jeffrey DeMunn",
-      "Larry Brandenburg",
-      "Neil Giuntoli",
-      "Brian Libby",
-      "David Proval",
-      "Joseph Ragno",
-      "Jude Ciccolella"
+    "color": [
+      "Black"
     ],
-    "name": "The Shawshank Redemption",
-    "year": 1994,
+    "name": "Hancock",
     "rating": 9.3,
     "votes": 1790841,
-    "image": "https://s3-eu-west-1.amazonaws.com/example-items/movies/5940bf19-4874-41a0-b458-30b5de9ca39b-orig.jpg",
+    "image": "https://sslimages1.shoppersstop.com/sys-master/images/he2/h14/9822389108766/202368469_9100.jpg_1088Wx1632H",
     "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "storyline": "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red. Written by J-S-Golden",
-    "director": [
-      "Frank Darabont"
-    ]
+    
  
   }
 ]
